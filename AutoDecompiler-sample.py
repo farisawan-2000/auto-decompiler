53a13d98d9ed89dd981d3f26e66d837a6e1f6c60
farisawan-2000 <faris@SIRAF2.localdomain>
2020-05-15 04:55:06 -0400

    Added sample decompiler script for mk64

---
+++ AutoDecompiler-sample.py
@@ -0,0 +1,13 @@
+import sys, os, glob
+
+# change these parameters as you please! :)
+REPO_DIRECTORY = "../mk64_2"
+NON_MATCHING_DIR = "../mk64_2/asm/non_matchings"
+ASM_DIR = "../mk64_2/asm/"
+SRC_DIR = "../mk64_2/src/"
+
+
+s_files = [i for i in glob.glob(ASM_DIR+"**/*.s", recursive=True) if "non_matchings" not in i]
+
+for i in s_files:
+	os.system("python3 AutoSplitter2.py "+i+" "+NON_MATCHING_DIR)
