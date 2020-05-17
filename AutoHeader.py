3353685017c05002e0cd862b45086e3641934b93
farisawan-2000 <faris@SIRAF2.localdomain>
2020-05-17 00:05:03 -0400

    more AutoHeader work

--- AutoHeader.py
+++ AutoHeader.py
@@ -3,25 +3,30 @@ import sys, os, glob, subprocess, re
 # function name
 myPattern = re.compile(r'func_[0-9a-fA-F]{8}')
 
-# function name plus its args (might be useful)
-funcArgs = re.compile(r'func_[0-9a-fA-F]{8}\([0-9A-za-z ,\?]*\)')
+# the entire function prototype
+funcArgs = re.compile(r'\**func_[0-9a-fA-F]{8}\([0-9A-za-z ,\?]+\)')
 
 # Pass 1: Add func calls to structures
-# TODO: 2 structures per file: funcs the file has, and funcs the file calls
+# TODO: write found funcs into a header structure
+# TODO: do this for all C files
 
 ifdef = -1
 lineNum = 0
 
 myFileFuncs = {}
+myFilePrototypes = {}
+
 notMyFileFuncs = {}
+notMyFilePrototypes = {}
 
 with open(sys.argv[1]) as inFile:
 	for line in inFile:
 		matches = myPattern.findall(line)
+		prototype = funcArgs.findall(line)
 		if ifdef == 0:
+			print(line[:-2])
 			for match in matches:
 				myFileFuncs[match] = lineNum
-			print(line[:-1], matches)
 		elif len(matches) > 0:
 			for match in matches:
 				notMyFileFuncs[match] = lineNum
@@ -29,3 +34,6 @@ with open(sys.argv[1]) as inFile:
 			ifdef = 2
 		lineNum+=1
 		ifdef -= 1
+
+print(myFileFuncs)
+print(notMyFileFuncs)
