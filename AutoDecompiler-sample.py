d4b024f4bff33ee9c436979dc5d63fc5943928e3
farisawan-2000 <faris@SIRAF2.localdomain>
2020-05-21 20:49:07 -0400

    new mips_to_c version

--- AutoDecompiler-sample.py
+++ AutoDecompiler-sample.py
@@ -1,14 +1,14 @@
 import sys, os, glob, re
 
 # change these parameters as you please! :)
-REPO_DIRECTORY = "../mk64"
-NON_MATCHING_DIR = "../mk64/asm/non_matchings"
-ASM_DIR = "../mk64/asm/"
-SRC_DIR = "../mk64/src/"
+REPO_DIRECTORY = "../mk64-2"
+NON_MATCHING_DIR = "../mk64-2/asm/non_matchings"
+ASM_DIR = "../mk64-2/asm/"
+SRC_DIR = "../mk64-2/src/"
 
 
 s_files = [i for i in glob.glob(ASM_DIR+"**/*.s", recursive=True) if "non_matchings" not in i and "entry" not in i and "rom_header" not in i and "boot" not in i]
-
+s_files = [i for i in s_files if "libultra" not in s_files] # skip libultra until it's split into files
 
 # TODO: Add filter for add instruction
 PureAsmDetector = re.compile(r"\badd\b[^.]")
