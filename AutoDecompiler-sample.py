2f89dd64729badf8a69e1387c178106420d326a6
Matt Kempster <kidpixel@gmail.com>
2020-06-19 09:36:55 -0700

    Run black on the codebase

--- AutoDecompiler-sample.py
+++ AutoDecompiler-sample.py
@@ -9,20 +9,29 @@ ASM_DIR = "../mk64_priv/asm/"
 SRC_DIR = "../mk64_priv/src/"
 
 
-s_files = [i for i in glob.glob(ASM_DIR+"**/*.s", recursive=True) if "non_matchings" not in i and "entry" not in i and "rom_header" not in i and "boot" not in i]
-s_files = [i for i in s_files if "libultra" not in s_files] # skip libultra until it's split into files
+s_files = [
+    i
+    for i in glob.glob(ASM_DIR + "**/*.s", recursive=True)
+    if "non_matchings" not in i
+    and "entry" not in i
+    and "rom_header" not in i
+    and "boot" not in i
+]
+s_files = [
+    i for i in s_files if "libultra" not in s_files
+]  # skip libultra until it's split into files
 
 # TODO: Add filter for add instruction
 PureAsmDetector = re.compile(r"\badd\b[^.]")
 s_files_copy = s_files
 for i in s_files_copy:
-	tmpFile = open(i,'r')
-	buff = tmpFile.read();
-	matches = PureAsmDetector.findall(buff)
-	if len(matches) != 0:
-		s_files.remove(i)
+    tmpFile = open(i, "r")
+    buff = tmpFile.read()
+    matches = PureAsmDetector.findall(buff)
+    if len(matches) != 0:
+        s_files.remove(i)
 
 currFile = 1
 for i in s_files:
-	os.system("python3 AutoSplitter2.py "+i+" "+NON_MATCHING_DIR)
-	currFile+=1
+    os.system("python3 AutoSplitter2.py " + i + " " + NON_MATCHING_DIR)
+    currFile += 1
--- AutoHeader.py
+++ AutoHeader.py
@@ -1,10 +1,10 @@
 import sys, os, glob, subprocess, re
 
 # function name
-myPattern = re.compile(r'func_[0-9a-fA-F]{8}')
+myPattern = re.compile(r"func_[0-9a-fA-F]{8}")
 
 # the entire function prototype
-funcArgs = re.compile(r'\**func_[0-9a-fA-F]{8}\([0-9A-za-z ,\?]+\)')
+funcArgs = re.compile(r"\**func_[0-9a-fA-F]{8}\([0-9A-za-z ,\?]+\)")
 
 # Pass 1: Add func calls to structures
 # TODO: write found funcs into a header structure
@@ -20,20 +20,20 @@ notMyFileFuncs = {}
 notMyFilePrototypes = {}
 
 with open(sys.argv[1]) as inFile:
-	for line in inFile:
-		matches = myPattern.findall(line)
-		prototype = funcArgs.findall(line)
-		if ifdef == 0:
-			print(line[:-2])
-			for match in matches:
-				myFileFuncs[match] = lineNum
-		elif len(matches) > 0:
-			for match in matches:
-				notMyFileFuncs[match] = lineNum
-		if "#ifdef MIPS_TO_C" in line:
-			ifdef = 2
-		lineNum+=1
-		ifdef -= 1
+    for line in inFile:
+        matches = myPattern.findall(line)
+        prototype = funcArgs.findall(line)
+        if ifdef == 0:
+            print(line[:-2])
+            for match in matches:
+                myFileFuncs[match] = lineNum
+        elif len(matches) > 0:
+            for match in matches:
+                notMyFileFuncs[match] = lineNum
+        if "#ifdef MIPS_TO_C" in line:
+            ifdef = 2
+        lineNum += 1
+        ifdef -= 1
 
 print(myFileFuncs)
 print(notMyFileFuncs)
